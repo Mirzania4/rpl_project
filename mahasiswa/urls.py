310cb2fd445415793c71d73737bb5fa644f69137
Mirzania Ahza <anyasenja23@gmail.com>
2026-06-10 13:33:18 +0200

    fix urls redirect

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -2,10 +2,9 @@ from django.urls import path
 from . import views
 from django.views.generic import RedirectView
 
-path('', RedirectView.as_view(url='daftar/')),
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', RedirectView.as_view(url='daftar/')),
     path('daftar/', views.daftar_mahasiswa, name='daftar_mahasiswa'),
 ]
 
