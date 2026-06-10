9fd2ba6d310b5b228026769b0559c0f8f4f1f81b
Mirzania Ahza <anyasenja23@gmail.com>
2026-06-10 14:02:54 +0200

    fix urls redirect

--- mahasiswa/urls.py
+++ mahasiswa/urls.py
@@ -2,9 +2,18 @@ from django.urls import path
 from . import views
 from django.views.generic import RedirectView
 
-
 urlpatterns = [
     path('', RedirectView.as_view(url='daftar/')),
-    path('daftar/', views.daftar_mahasiswa, name='daftar_mahasiswa'),
-]
 
+    path('daftar/', views.daftar_mahasiswa, name='daftar_mahasiswa'),
+
+    path('tambah/', views.tambah_mahasiswa, name='tambah_mahasiswa'),
+
+    path('edit/<int:pk>/',
+         views.edit_mahasiswa,
+         name='edit_mahasiswa'),
+
+    path('hapus/<int:pk>/',
+         views.hapus_mahasiswa,
+         name='hapus_mahasiswa'),
+]
--- mahasiswa/views.py
+++ mahasiswa/views.py
@@ -1,11 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Mahasiswa
+from .forms import MahasiswaForm
 from django.contrib.auth.decorators import login_required
+
 
 @login_required(login_url='/accounts/login/')
 def daftar_mahasiswa(request):
     mahasiswas = Mahasiswa.objects.all()
-    return render(request, 'mahasiswa/daftar.html', {'mahasiswas': mahasiswas})
+    return render(request, 'mahasiswa/daftar.html', {
+        'mahasiswas': mahasiswas
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def tambah_mahasiswa(request):
+    if request.method == 'POST':
+        form = MahasiswaForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('daftar_mahasiswa')
+
+    else:
+        form = MahasiswaForm()
+
+    return render(request, 'mahasiswa/form.html', {
+        'form': form,
+        'judul': 'Tambah Mahasiswa'
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def edit_mahasiswa(request, pk):
+    mahasiswa = get_object_or_404(Mahasiswa, pk=pk)
+
+    if request.method == 'POST':
+        form = MahasiswaForm(request.POST, instance=mahasiswa)
+
+        if form.is_valid():
+            form.save()
+            return redirect('daftar_mahasiswa')
+
+    else:
+        form = MahasiswaForm(instance=mahasiswa)
+
+    return render(request, 'mahasiswa/form.html', {
+        'form': form,
+        'judul': 'Edit Mahasiswa'
+    })
+
+
+@login_required(login_url='/accounts/login/')
+def hapus_mahasiswa(request, pk):
+    mahasiswa = get_object_or_404(Mahasiswa, pk=pk)
+
+    mahasiswa.delete()
+
+    return redirect('daftar_mahasiswa')
 
 
 def index(request):
@@ -14,9 +65,3 @@ def index(request):
         'deskripsi': 'Contoh halaman index menggunakan Django templates dan static files.'
     }
     return render(request, 'mahasiswa/index.html', context)
-
-# from django.http import HttpResponse
-# def index(request):
-#     return HttpResponse("Hello, ini modul praktikum RPL Django!")
-# # Create your views here.
-
